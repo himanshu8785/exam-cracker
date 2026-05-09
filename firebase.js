ab6c69fc77122d63aef23af2890fba77237c7f16
Himanshu <himanshusingh998454@gmail.com>
2026-05-09 13:16:08 +0530

    hard reset firebase

--- firebase.js
+++ firebase.js
@@ -8,11 +8,10 @@ const firebaseConfig = {
   projectId: "exam-cracker-16",
   storageBucket: "exam-cracker-16.firebasestorage.app",
   messagingSenderId: "278430144732",
-  appId: "1:278430144732:web:332a08959b458c955fbc19"
+  appId: "1:278430144732:web:332a08959b458c955fbc19",
 };
 
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
-
 export const db = getFirestore(app);
