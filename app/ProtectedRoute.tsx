f59ee6d41dca5298f868979d315b9a3c5b2ce5b0
Himanshu <himanshusingh998454@gmail.com>
2026-05-10 09:45:36 +0530

    fixed protected route types

--- app/ProtectedRoute.tsx
+++ app/ProtectedRoute.tsx
@@ -4,15 +4,28 @@ import { useEffect,useState } from "react";
 
 import { useRouter } from "next/navigation";
 
-import { onAuthStateChanged } from "firebase/auth";
+import {
+  onAuthStateChanged
+} from "firebase/auth";
 
-import { auth } from "../firebase";
+import {
+  auth
+} from "../firebase";
 
-export default function ProtectedRoute({ children }) {
+export default function ProtectedRoute({
 
-  const [loading,setLoading] = useState(true);
+  children,
+
+}:{
+
+  children:React.ReactNode
+
+}) {
 
   const router = useRouter();
+
+  const [loading,setLoading] =
+  useState(true);
 
   useEffect(()=>{
 
@@ -25,13 +38,17 @@ export default function ProtectedRoute({ children }) {
 
       }
 
-      setLoading(false);
+      else{
+
+        setLoading(false);
+
+      }
 
     });
 
     return ()=>unsubscribe();
 
-  },[]);
+  },[router]);
 
   if(loading){
 
@@ -39,7 +56,7 @@ export default function ProtectedRoute({ children }) {
 
       <main className="min-h-screen bg-[#0b1120] flex justify-center items-center text-white">
 
-        <h1 className="text-3xl font-bold">
+        <h1 className="text-4xl font-bold">
 
           Loading...
 
