82c449e2454092aa6e6d36765eb248c4b88cf003
Himanshu <himanshusingh998454@gmail.com>
2026-05-10 10:29:15 +0530

    fixed next config

--- next.config.js
+++ next.config.js
@@ -1,13 +1,9 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-
   typescript: {
-
     ignoreBuildErrors: true,
-
   },
-
 };
 
-export default nextConfig;
+module.exports = nextConfig;
